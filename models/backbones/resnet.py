1501dbe0784f71822cb67ff688737f3b84f8d29c
Lưu Hoàng Minh <hoangminhhy2k5@gmail.com>
2026-02-07 22:56:35 +0700

    Initial commit

--- models/backbones/resnet.py
+++ models/backbones/resnet.py
@@ -9,9 +9,10 @@ class ResNetBackbone(nn.Module):
     Return C3 C4 C5 feature maps
     """
 
-    def __init__(self, name="resnet18", pretrained=True):
+    def __init__(self, name="resnet18", pretrained=True, in_channels=6):
         super().__init__()
 
+        # ===== load resnet =====
         if name == "resnet18":
             net = models.resnet18(weights=models.ResNet18_Weights.DEFAULT if pretrained else None)
             self.out_channels = [128, 256, 512]
@@ -23,13 +24,24 @@ class ResNetBackbone(nn.Module):
         else:
             raise ValueError("Only support resnet18 / resnet50")
 
-        # stem
-        self.conv1 = net.conv1
+        # ===== FIX CONV1 FOR 6 CHANNEL INPUT =====
+        old_conv = net.conv1
+        self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
+
+        if pretrained:
+            with torch.no_grad():
+                # old weight: [64, 3, 7, 7]
+                self.conv1.weight[:, :3] = old_conv.weight
+
+                # copy RGB mean to extra channels
+                mean_weight = old_conv.weight.mean(dim=1, keepdim=True)
+                self.conv1.weight[:, 3:] = mean_weight
+
+        # ===== keep rest =====
         self.bn1 = net.bn1
         self.relu = net.relu
         self.maxpool = net.maxpool
 
-        # stages
         self.layer1 = net.layer1
         self.layer2 = net.layer2  # C3
         self.layer3 = net.layer3  # C4
